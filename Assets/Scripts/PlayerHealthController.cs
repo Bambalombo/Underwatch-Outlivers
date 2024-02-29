574e634d24e325d854a5415d1619f25c97fe3174
kjulsg21 <100912975+kjulsg21@users.noreply.github.com>
2024-02-29 19:43:13 +0100

    Death text :alien:

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealthController : MonoBehaviour
 {
     [SerializeField] private StatusBarController healthbarController;
+    [SerializeField] private TextMeshProUGUI deathText;
     [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float currentHealth;
+    [SerializeField] private float enemyDamage = 9f;
+
 
     private void Start()
     {
         currentHealth = maxHealth;
+        deathText = GameObject.FindWithTag("DeathTextTag").GetComponent<TextMeshProUGUI>();
+        deathText.enabled = false;
     }
-
+    
+    //TODO: Death needs to be when every player is dead, not just one 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         healthbarController.UpdateStatusBar(currentHealth, maxHealth);
-        if (currentHealth <= 0)
+        if (currentHealth < 0) 
         {
+            deathText.enabled = true;
+            deathText.text = "You Died!";
             // Handle player death here
         }
     }
@@ -27,7 +36,7 @@ public class PlayerHealthController : MonoBehaviour
         if (enemy != null)
         {
             // Assume the enemy deals a fixed amount of damage
-            TakeDamage(10f);
+            TakeDamage(enemyDamage);
         }
     }
 }
