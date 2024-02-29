8f6625e2c630f0b3d15c08a56a20f5521e6e2595
kjulsg21 <100912975+kjulsg21@users.noreply.github.com>
2024-02-29 01:07:25 +0100

    Display game time at the top of the screen

---
+++ Assets/Scripts/DisplayGameTime.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameTimeDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI  timeText;
+    private float gameTime;
+
+    void Start()
+    {
+        gameTime = 0;
+        
+        timeText = GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+        UpdateTimeText();
+    }
+
+    void UpdateTimeText()
+    {
+        int minutes = Mathf.FloorToInt(gameTime / 60);
+        int seconds = Mathf.FloorToInt(gameTime % 60);
+        timeText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
+}
