5d79b7d82f7a636828967ee64449a5d30b8ebd2e
Julsgaard <100912975+Julsgaard@users.noreply.github.com>
2024-03-20 02:10:54 +0100

    I got an error for some reason so had to remove this

--- Assets/Scripts/5. Ability/ChainLightning.cs
+++ Assets/Scripts/5. Ability/ChainLightning.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class ChainLightning : MonoBehaviour
